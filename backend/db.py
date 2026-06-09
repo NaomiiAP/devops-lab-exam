fd45c9f01cfbced1d168f2d15435424193315059
Naomi P <pereiranaomii44@gmail.com>
2026-06-09 20:06:33 +0530

    done done

--- backend/db.py
+++ backend/db.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 
 # MongoDB Atlas connection placeholder
 # In a real app, use environment variables
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://<username>:<password>@cluster0.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URI = os.getenv("MONGO_URI", ".")
 client = MongoClient(MONGO_URI)
 db = client['taskflow']
+
